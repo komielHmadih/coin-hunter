6975932db451dc1d5d6286c5d1144f82fb402dec
komielHmadih <73136329+komielHmadih@users.noreply.github.com>
2021-11-30 14:53:25 +0200

    add initial files

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "../components/Banner/Banner";
+import CoinsTable from "../components/CoinsTable";
+import CoinsTable2 from "../components/CoinsTable2";
+
+const Homepage = () => {
+  return (
+    <>
+      <Banner />
+      <CoinsTable  />
+      <CoinsTable2 />
+    </>
+  );
+};
+
+export default Homepage;
